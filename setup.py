28b51789d7807cb17ee9e14a55b548c999919afa
Bryan Larson <b32147@gmail.com>
2020-05-13 07:37:45 -0400

    DBMISVC-63 - Added GitHub workflows; updated setup.py file; fixed tests; other pre-commit/linting tweaks

--- setup.py
+++ setup.py
@@ -2,26 +2,29 @@
 
 
 from codecs import open
-from os.path import abspath, dirname, join
 from subprocess import call
 
 from setuptools import Command, find_packages, setup
 
-from stack import __version__
-
-
-this_dir = abspath(dirname(__file__))
-with open(join(this_dir, 'README.md'), encoding='utf-8') as file:
-    long_description = file.read()
+from stack import (
+    __title__,
+    __description,
+    __author__,
+    __author_email__,
+    __license__,
+    __url__,
+    __version__,
+)
 
 
 def read(f):
-    return open(f, 'r', encoding='utf-8').read()
+    return open(f, "r", encoding="utf-8").read()
 
 
 class RunTests(Command):
     """Run all tests."""
-    description = 'run tests'
+
+    description = "run tests"
     user_options = []
 
     def initialize_options(self):
@@ -32,46 +35,43 @@ class RunTests(Command):
 
     def run(self):
         """Run all tests!"""
-        errno = call(['py.test', '--cov=stack', '--cov-report=term-missing'])
+        errno = call(["py.test", "--cov=stack", "--cov-report=term-missing"])
         raise SystemExit(errno)
 
 
 setup(
-    name='hmsdbmitc-stack',
+    name=__title__,
     version=__version__,
-    description='A command line program to manage a Docker stack.',
-    long_description=long_description,
-    url='https://github.com/hms-dbmi/stack.git',
-    author='HMS DBMI Tech-core',
-    author_email='bryan_larson@hms.harvard.edu',
-    license='UNLICENSE',
+    description=__description,
+    long_description=read("README.md"),
+    url=__url__,
+    author=__author__,
+    author_email=__author_email__,
+    license=__license__,
     classifiers=[
-        'Intended Audience :: Developers',
-        'Topic :: Utilities',
-        'License :: Public Domain',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "License :: Public Domain",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords='cli',
-    packages=find_packages(exclude=['docs', 'tests*']),
-    install_requires=read('requirements.txt').splitlines(),
+    keywords="cli",
+    packages=find_packages(exclude=["docs", "tests*"]),
+    install_requires=read("requirements.txt").splitlines(),
     extras_require={
-        'test': read('requirements-test.txt').splitlines(),
-        'dev': read('requirements-test.txt').splitlines() + read('requirements-dev.txt').splitlines()
+        "selenium": read("requirements-selenium.txt").splitlines(),
+        "test": read("requirements-test.txt").splitlines(),
+        "dev": read("requirements-test.txt").splitlines()
+        + read("requirements-dev.txt").splitlines()
+        + read("requirements-selenium.txt").splitlines(),
     },
-    entry_points={
-        'console_scripts': [
-            'stack=stack.cli:main',
-        ],
-    },
-    cmdclass={'test': RunTests},
+    entry_points={"console_scripts": ["stack=stack.cli:main",],},
+    cmdclass={"test": RunTests},
 )
--- stack/__init__.py
+++ stack/__init__.py
@@ -1,1 +1,29 @@
-__version__ = '0.5.0'
+"""
+DBMISVC Stack
+"""
+from datetime import datetime
+year = datetime.now().strftime("%Y")
+
+__title__ = "DBMISVC Stack"
+__description = (
+    "A command line program to facilitate development "
+    "on a stack of containerized services"
+)
+__version__ = "0.5.0"
+__url__ = "https://github.com/hms-dbmi/stack.git"
+__author__ = "HMS DBMI Technology Core"
+__author_email__ = "bryan_larson@hms.harvard.edu"
+__license__ = "BSD 2-Clause"
+__copyright__ = (
+    "Copyright 2011-{} Harvard Medical School"
+    " Department of Biomedical Informatics".format(year)
+)
+
+# Version synonym
+VERSION = __version__
+
+# Header encoding (see RFC5987)
+HTTP_HEADER_ENCODING = "iso-8859-1"
+
+# Default datetime input and output formats
+ISO_8601 = "iso-8601"
